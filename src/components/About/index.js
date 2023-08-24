181e088436130e684ab91fe9abba4defd66e58d4
ashok <aashok59238@gmail.com>
2023-08-24 07:20:15 +0000

    frontendtask

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <h1 className="about-heading">About</h1>
+    <p className="about-paragraph">
+      I love to create! I am a frontend web developer
+    </p>
+  </div>
+)
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+// Write your JS code here
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <h1 className="home-heading">Home</h1>
+  </div>
+)
+
+export default Home
